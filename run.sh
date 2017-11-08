5ab50fd5cb304ffd0d5b527e15fb383aa1aca718
mohitepramod <30810865+mohitepramod@users.noreply.github.com>
2017-11-08 21:49:36 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 "#!/bin/bash
 
-wget http://192.168.56.104:8081/repository/maven-snapshots/Sample/sample/maven-metadata.xml -O baseVersion.xml
+wget http://192.168.56.104:8081/repository/maven-snapshots/Sample/sample/maven-metadata.xml -O ./opt/baseVersion.xml
 
 TEMP_VERSION=`grep \<version\> ./baseVersion.xml `
 echo "\$TEMP_VERSION"
@@ -13,7 +13,7 @@ BASE_VER=$(echo "\${BASE_VERSION}" | tail -n1)
 
 echo "\$BASE_VER"
 
-wget 'http://192.168.56.104:8081/repository/maven-snapshots/Sample/sample/'\${BASE_VER}'/maven-metadata.xml' -O artifactVersion.xml
+wget 'http://192.168.56.104:8081/repository/maven-snapshots/Sample/sample/'\${BASE_VER}'/maven-metadata.xml' -O ./opt/artifactVersion.xml
 
 TEMP_VERSION=`grep -m 1 \<value\> ./artifactVersion.xml`
 echo "\$TEMP_VERSION"
@@ -21,4 +21,4 @@ echo "\$TEMP_VERSION"
 FINAL_VERSION=$(echo "\${TEMP_VERSION}" | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
 echo "\$FINAL_VERSION"
 
-wget 'http://192.168.56.104:8081/repository/maven-snapshots/Sample/sample/'\${BASE_VER}'/sample-'\${FINAL_VERSION}'.jar' -O  sample.jar"
+wget 'http://192.168.56.104:8081/repository/maven-snapshots/Sample/sample/'\${BASE_VER}'/sample-'\${FINAL_VERSION}'.jar' -O  ./opt/sample.jar"
