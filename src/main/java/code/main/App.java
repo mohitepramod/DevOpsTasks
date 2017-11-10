46aac609a7ce16d5977bf96df5960827e3b73c98
mohitepramod <30810865+mohitepramod@users.noreply.github.com>
2017-11-10 10:40:29 +0530

    Update App.java

--- src/main/java/code/main/App.java
+++ src/main/java/code/main/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Pramod!" );
+        System.out.println( "Hello Pramod! from updated version of code......" );
     }
 }
